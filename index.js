dad8a52321a67477aced8228f6a23ae46d0603f0
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2023-05-27 23:56:33 +0530

    database added

--- index.js
+++ index.js
@@ -1,67 +1,83 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/noteModel");
 const app = express();
-let notes = [
-  { id: 1, content: "HTML is easy", important: true },
-  { id: 2, content: "Browser can execute only JavaScript", important: false },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
-const generateId = () => {
-  const maxId = Math.max(...notes.map((note) => note.id));
-  return maxId + 1;
-};
-
+app.use(express.static("./build"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("./build"));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((result) => {
+    res.json(result);
+  });
 });
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const resource = notes.find((note) => note.id === id);
-  if (resource) {
-    res.json(resource);
-  } else {
-    res.status(404).send("Not found!");
-  }
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) res.json(note);
+      else res.status(404).end();
+    })
+    .catch((err) => next(err));
 });
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
   const body = req.body;
-  if (!body.content) {
-    return res.status(400).json({ error: "content missing" });
-  }
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
+  });
+
+  note
+    .save()
+    .then((result) => res.json(result))
+    .catch((err) => next(err));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const updatedNote = {
+    content: req.body.content,
+    important: req.body.important,
   };
-  notes = notes.concat(note);
-  res.json(note);
+  Note.findByIdAndUpdate(req.params.id, updatedNote, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => next(err));
 });
 
-app.put("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const updatedNote = req.body;
-  notes = notes.map((n) => (n.id !== id ? n : updatedNote));
-  res.status(200).json(updatedNote);
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      if (result === null) {
+        return res.status(404).json({ error: "already deleted" });
+      }
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
-});
+const unknownEndpoint = (req, res) => {
+  res.status(404).json({ error: "Unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  if (err.name === "CastError") {
+    return res.status(400).json({ error: "Malformatred id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message });
+  }
+  next(err);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
---
+++ models/noteModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const password = process.env.DB_PASS;
+let url = process.env.MONGODB_URI;
+url = url.replace("<password>", password);
+console.log("connecting to", url);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
